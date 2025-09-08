35d0c1a3b2d3a9dd5f0fc0a2a1dd68990a916673
deeprtm3997@gmail.com <deeprtm3997@gmail.com>
2025-09-08 07:28:18 +0000

    Desffgh

--- prophecy/pipelines/pp002.py
+++ prophecy/pipelines/pp002.py
@@ -1,9 +1,17 @@
+Config = {"abc" : "'t1'"}
 Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
 SensorSchedule = SensorSchedule(enabled = False)
 
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    t1 = Task(
-        task_id = "t1", 
+with DAG(Config = Config, Schedule = Schedule, SensorSchedule = SensorSchedule):
+    varabc_1 = Task(
+        task_id = "varabc_1", 
         component = "Dataset", 
-        table = {"name" : "t1", "sourceType" : "Source", "sourceName" : "deeptanshu.default", "alias" : ""}
+        table = {
+          "name": "{{var('abc')}}", 
+          "sourceType": "Table", 
+          "sourceName": "deeptanshu.default", 
+          "alias": "", 
+          "additionalProperties": None
+        }, 
+        writeOptions = {"writeMode" : "overwrite"}
     )
